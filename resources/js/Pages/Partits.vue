14a2515fed5f8e4cd085bf540e165465de244fa3
adriajofre19 <118483942+adriajofre19@users.noreply.github.com>
2024-05-24 12:43:22 +0200

    a

--- resources/js/Pages/Partits.vue
+++ resources/js/Pages/Partits.vue
@@ -33,94 +33,92 @@ const filtrarPartits = () => {
             <option v-for="arbitre in arbitres" :key="arbitre.id" :value="arbitre">{{ arbitre }}</option>
         </select>
 
-        <div v-if="partitsArbitreSeleccionat != ''">
-            <table class="table-auto">
-                <thead>
+        <div class="relative overflow-x-auto">
+            <table class="text-sm text-left rtl:text-right text-gray-500">
+                <thead class="text-xs bg-gray-800 text-white">
                     <tr>
-                        <th>Local</th>
-                        <th>Targetas Jug Local</th>
-                        <th>Targetas Cp Local</th>
-                        <th>Resultat</th>
-                        <th>Visitant</th>
-                        <th>Àrbitre</th>
+                        <th scope="col" class="px-6 py-3">
+                            Jorndada
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Partido
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Ganador
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Tarjetas Local
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Tarjetas Visitante
+                        </th>
+
                     </tr>
                 </thead>
                 <tbody>
-                    <!-- Loop through the partits array and generate a row for each one -->
-                    <tr v-for="partit in partitsArbitreSeleccionat" :key="partit.id">
-                        <td>{{ partit.equipoLocal }}</td>
-                        <td>Amarillas: {{ partit.AmarillasJugadoresLocal }} Rojas: {{ partit.RojasJugadoresLocal }}</td>
-                        <td>Amarillas: {{ partit.AmarillasCuerpoTecnicoLocal }} Rojas: {{ partit.RojasCuerpoTecnicoLocal
-                            }}</td>
-                        <td>{{ partit.resLocal }} - {{ partit.resVisitant }}</td>
-                        <td>{{ partit.equipoVisitant }}</td>
-                        <td>{{ partit.arbitre }}</td>
+                    <tr v-for="partido in partitsArbitreSeleccionat" :key="partido.id" class="bg-white border-b">
+                        <td class="px-6 py-4">
+                            {{ partido.jornada }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ partido.equipoLocal }} <span class="bg-gray-300 p-1 rounded-md mx-2">{{ partido.resLocal
+                                }} -
+                                {{
+                                    partido.resVisitant }} </span> {{
+                                    partido.equipoVisitant }}
+                        </td>
+
+                        <td class="px-6 py-4">
+                            <span v-if="partido.ganador === 'Local'" class="bg-orange-200 p-1 rounded-md">{{
+                                partido.ganador }}
+                            </span>
+                            <span v-else-if="partido.ganador === 'Visitante'" class="bg-green-200 p-1 rounded-md">{{
+                                partido.ganador }}</span>
+                            <span v-else class="bg-yellow-200 p-1 rounded-md">Empate</span>
+                        </td>
+
+                        <td class="px-6 py-4">
+                            <span class="bg-yellow-400 py-1 px-2 mr-2 text-black rounded-md"> {{
+                                partido.AmarillasJugadoresLocal }}</span>
+                            <span class="bg-red-400 py-1 px-2 mr-2 text-black rounded-md"> {{
+                                partido.RojasJugadoresLocal }}</span>
+                            <span class="bg-yellow-400 py-1 px-2 mr-2 text-black rounded-md"> {{
+                                partido.AmarillasCuerpoTecnicoLocal }}</span>
+                            <span class="bg-red-400 py-1 px-2 text-black rounded-md"> {{ partido.RojasCuerpoTecnicoLocal
+                                }}</span>
+                        </td>
+
+                        <td class="px-6 py-4">
+                            <span class="bg-yellow-400 py-1 px-2 mr-2 text-black rounded-md"> {{
+                                partido.AmarillasJugadoresVisitante }}</span>
+                            <span class="bg-red-400 py-1 px-2 mr-2 text-black rounded-md"> {{
+                                partido.RojasJugadoresVisitante
+                                }}</span>
+                            <span class="bg-yellow-400 py-1 px-2 mr-2 text-black rounded-md"> {{
+                                partido.AmarillasCuerpoTecnicoVisitante
+                            }}</span>
+                            <span class="bg-red-400 py-1 px-2 text-black rounded-md"> {{
+                                partido.RojasCuerpoTecnicoVisitante
+                                }}</span>
+                        </td>
+
                     </tr>
                 </tbody>
             </table>
         </div>
 
-
-
-
-
-
-        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                    <tr>
-                        <th scope="col" class="px-6 py-3">
-                            Local
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Resultat
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Visitant
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Price
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Action
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="partit in partitsArbitreSeleccionat"
-                        class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                        <th scope="row"
-                            class="px-6 flex py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            {{ partit.equipoLocal }}
-                            <svg class="w-6 h-6 text-yellow-500" style="color: yellow;" aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
-                                viewBox="0 0 24 24">
-                                <path fill-rule="evenodd"
-                                    d="M4 4c0-.975.718-2 1.875-2h12.25C19.282 2 20 3.025 20 4v16c0 .975-.718 2-1.875 2H5.875C4.718 22 4 20.975 4 20V4Zm7 13a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2h-2Z"
-                                    clip-rule="evenodd" />
-                            </svg> {{ partit.AmarillasJugadoresLocal }}
-
-
-                        </th>
-                        <td class="px-6 py-4">
-                            {{ partit.resLocal }} - {{ partit.resVisitant }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ partit.equipoVisitant }}
-                        </td>
-                        <td class="px-6 py-4">
-                            $2999
-                        </td>
-                        <td class="px-6 py-4">
-                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
 
 
 
 
     </AuthenticatedLayout>
 </template>
+
+<style scoped>
+.yellow-card {
+    background-color: yellow;
+    width: 20px;
+    height: 25px;
+    color: black;
+}
+</style>
